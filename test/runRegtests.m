ee0a19ac053f6b91ca87ffb90cf257ce925b0c9f
Pablo Romero <promero@mathworks.com>
2022-11-07 12:56:43 +0100

    Fix regression tests for github.

--- test/runRegtests.m
+++ test/runRegtests.m
@@ -1,13 +1,30 @@
+function runRegtests(tags)
+
+arguments
+    tags {mustBeText} = '';
+end
+
 import matlab.unittest.TestRunner;
 import matlab.unittest.TestSuite;
 import matlab.unittest.Verbosity;
 import matlab.unittest.plugins.XMLPlugin;
+import matlab.unittest.selectors.HasTag;
 
 projObj = currentProject;
 
 suite = TestSuite.fromProject(projObj);
+if ~isempty(tags)
+    suite = suite.selectIf(HasTag(tags));
+end
 
 runner = TestRunner.withTextOutput('OutputDetail', Verbosity.Detailed);
 runner.addPlugin(XMLPlugin.producingJUnitFormat(fullfile(projObj.RootFolder,'results.xml')));
 
 results = runner.run(suite);
+
+if ~ismember('github',tags)
+    % GitHub actions evaluate test success from Test Report
+    disp(results.assertSuccess);
+end
+
+end
